e4be644d0af7ecb620017c8dc3c0a73d63774782
mikosken <89968969+mikosken@users.noreply.github.com>
2022-03-31 18:18:00 +0200

    chore: "Created new ASP.NET Core web app (.NET 6.0) in Visual Studio."

---
+++ FileUploadToDisk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FileUploadToDisk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
