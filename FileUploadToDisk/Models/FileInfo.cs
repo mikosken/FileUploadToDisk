9622c76b309dc4c879f7838fbb401eedba31c721
mikosken <89968969+mikosken@users.noreply.github.com>
2022-03-31 18:52:19 +0200

    feat: Added FileInfo model class.

---
+++ FileUploadToDisk/Models/FileInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace FileUploadToDisk.Models
+{
+    public class FileInfo
+    {
+        int Id { get; set; }
+        string? OriginalFilename { get; set; }
+
+        // Generated/safe filename.
+        string? Filename { get; set; }
+
+        DateTime UploadedDateTime { get; set; } = DateTime.Now;
+        long FileSize { get; set; }
+    }
+}
